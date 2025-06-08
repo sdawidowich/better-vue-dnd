19a843b5395a695124487ad6fb7c6fcf1fc3229a
sdawidowich <79337885+sdawidowich@users.noreply.github.com>
2025-06-07 23:26:30 -0500

    Initial files

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <div>
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/examples">Examples</RouterLink>
+            </nav>
+        </div>
+    </header>
+    <RouterView />
+</template>
---
+++ src/components/dnd/Draggable.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    const props = withDefaults(defineProps<{
+        handle?: boolean;
+    }>(), {
+        handle: false
+    });
+</script>
+
+<template>
+    <div>
+        <slot />
+    </div>
+</template>
